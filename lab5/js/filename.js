3786867bbc0c105fda66230dc7054d03108dcbe3
Ethan Fuller <etafulle@ucsc.edu>
2023-04-27 03:32:54 -0700

    I worked Data and Variables in coding.

---
+++ lab5/js/filename.js
@@ -0,0 +1,20 @@
+// index.js - Practice with data and variables
+// Author: Ethan Fuller
+// Date: 4/26/2023
+
+
+//Define Variables
+make = "McLaren";
+model = "GT";
+color = "Green";
+year = 2019;
+
+//calculate
+age = 2023 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
